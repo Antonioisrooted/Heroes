c57cfeb5f1d23bfdefaffaf333f7fe9694b510ae
Antonioisrooted <antoniommaina@gmail.com>
2020-04-27 08:54:16 +0300

    additional postgresql file to libraries

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,43 @@
+import java.util.ArrayList;
+
 import static spark.Spark.*;
 
 public class App {
-    public static void main(String[] args) {
-        get("/hello", (request, response) -> "Hello Friend!");
+    String name;
+    int age;
+    String sex;
+    boolean intact;
+    ArrayList<String> breeds;
+
+    public App(String name, int age, String sex, boolean intact, ArrayList<String> breeds) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+        this.intact = intact;
+        this.breeds = breeds;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public App(String name) {
+        this.name = name;
+    }
+
+    public boolean isIntact() {
+        return intact;
+    }
+
+    public ArrayList<String> getBreeds() {
+        return breeds;
     }
 }
