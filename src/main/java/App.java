bf387302262ed56c6a1df7114b72cf9129f9e5ba
Antonioisrooted <antoniommaina@gmail.com>
2020-04-28 07:27:32 +0300

    builded files

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -21,7 +21,7 @@ public class App {
         if (process.environment().get("PORT") != null) {
             port = Integer.parseInt(process.environment().get("PORT"));
         } else {
-            port = 8080;
+            port = 4567;
         }
 
         port(port);
